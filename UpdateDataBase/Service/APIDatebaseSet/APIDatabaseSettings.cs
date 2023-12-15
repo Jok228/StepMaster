b1962415043a2379650f7e704bd858ba0e7753b2
FrankDEBILua <sosiska6742@gmail.com>
2023-12-15 20:00:21 +0700

    Ladno

--- UpdateDataBase/Service/APIDatebaseSet/APIDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.Services.ForDb.APIDatebaseSet
-{
-    public class ApiDatabaseSettings : IAPIDatabaseSettings
-    {
-        public string NameCollection { get; set; } = string.Empty;
-        public string ConnectionString { get; set; } = string.Empty;
-        public string DatabaseName { get; set; } = string.Empty;
-    }
-}
